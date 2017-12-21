a086ec6550c407ee3c83089f437466d473a257bc
SKY-20171204MBM\Administrator <992767879@qq.com>
2017-12-21 16:14:19 +0800

    Initial commit

---
+++ app/src/main/java/cn/qqtheme/androidpicker/MyPickerApp.java
@@ -0,0 +1,24 @@
+package cn.qqtheme.androidpicker;
+
+import android.app.Application;
+
+import cn.qqtheme.framework.AppConfig;
+import cn.qqtheme.framework.util.LogUtils;
+
+/**
+ * Author:李玉江[QQ:1032694760]
+ * DateTime:2016/7/20 20:28
+ * Builder:Android Studio
+ */
+public class MyPickerApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        LogUtils.setIsDebug(BuildConfig.DEBUG);
+        if (!LogUtils.isDebug()) {
+            android.util.Log.d(AppConfig.DEBUG_TAG, "logcat is disabled");
+        }
+    }
+
+}
